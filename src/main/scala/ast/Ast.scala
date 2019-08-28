aac56266190171b4108389cd8518a677e76d682a
Hannes Tenter <tenter@e-spirit.com>
2019-08-28 13:12:50 +0200

    working state of more complex nested statements and expressions

--- src/main/scala/ast/Ast.scala
+++ src/main/scala/ast/Ast.scala
@@ -1,14 +1,12 @@
 package ast
 
-import java.util
-
-import de.hanno.kotlin.KotlinParser.{FunctionBodyContext, KotlinFileContext}
+import de.hanno.kotlin.KotlinParser._
 import de.hanno.kotlin.{KotlinParser, KotlinParserBaseListener}
 import lexerparser.{KotlinFileTreeWalker, LexerParser}
-import org.antlr.v4.runtime.tree.TerminalNode
 
 import scala.collection.mutable
 import scala.collection.mutable.ListBuffer
+import scala.util.Try
 
 class AstCreator extends KotlinParserBaseListener {
 
@@ -26,18 +24,26 @@ class AstCreator extends KotlinParserBaseListener {
 
     new KotlinFileTreeWalker(file).walk(new KotlinParserBaseListener() {
       override def enterFunctionDeclaration(ctx: KotlinParser.FunctionDeclarationContext): Unit = {
-        val function = Function(name = ctx.simpleIdentifier().getText, parent = stack.headOption)
-        stack.headOption.map(_.children += function)
-        if(isTopLevel) instructions += function
-        stack.push(function)
+        val isClassMember = ctx.getParent.getParent.isInstanceOf[ClassMemberDeclarationContext]
+        if(!isClassMember) return
+        enterDeclareFunction(instructions, stack, isTopLevel _, ctx)
       }
 
       override def exitFunctionDeclaration(ctx: KotlinParser.FunctionDeclarationContext): Unit = {
-        stack.pop()
+        val isClassMember = ctx.getParent.getParent.isInstanceOf[ClassMemberDeclarationContext]
+        if(!isClassMember) return
+        exitDeclareFunction(stack)
       }
 
       override def enterFunctionBody(ctx: KotlinParser.FunctionBodyContext): Unit = {
+        val isClassMember = !ctx.getParent.getParent.isInstanceOf[DeclarationContext]
+        if(!isClassMember) return
         stack.headOption.map(_.asInstanceOf[Function]).foreach(_.bodyType = FunctionBodyType(ctx))
+      }
+      override def exitFunctionBody(ctx: KotlinParser.FunctionBodyContext): Unit = {
+        val isClassMember = !ctx.getParent.getParent.isInstanceOf[DeclarationContext]
+        if(!isClassMember) return
+        stack.pop
       }
 
       override def enterClassDeclaration(ctx: KotlinParser.ClassDeclarationContext): Unit = {
@@ -62,9 +68,39 @@ class AstCreator extends KotlinParserBaseListener {
       override def exitPropertyDeclaration(ctx: KotlinParser.PropertyDeclarationContext): Unit = {
         stack.pop
       }
+
+      override def enterExpression(ctx: KotlinParser.ExpressionContext): Unit = {
+        stack.headOption.map(it => it.children += ExpressionType(ctx, Some(it)))
+      }
+
+      override def exitExpression(ctx: KotlinParser.ExpressionContext): Unit = {
+      }
+
+      override def enterStatement(ctx: KotlinParser.StatementContext): Unit = {
+        Option(ctx.declaration()).map(_.functionDeclaration()).map { it =>
+          enterDeclareFunction(instructions, stack, isTopLevel _, it)
+        }
+      }
+      override def exitStatement(ctx: KotlinParser.StatementContext): Unit = {
+        Option(ctx.declaration()).map(_.functionDeclaration()).map { it =>
+          exitDeclareFunction(stack)
+        }
+      }
+
     })
 
     instructions.toList
+  }
+
+  private def exitDeclareFunction(stack: mutable.Stack[Instruction]) = {
+    stack.pop()
+  }
+
+  private def enterDeclareFunction(instructions: ListBuffer[Statement], stack: mutable.Stack[Instruction], isTopLevel: () => Boolean, ctx: FunctionDeclarationContext) = {
+    val function = Function(name = ctx.simpleIdentifier().getText, parent = stack.headOption)
+    stack.headOption.map(_.children += function)
+    if (isTopLevel()) instructions += function
+    stack.push(function)
   }
 }
 
@@ -80,6 +116,11 @@ case class Function(val name: String, var bodyType: FunctionBodyType = RegularFu
 case class Clazz(name: String, override val parent: Option[Instruction]) extends Declaration
 case class Property(name: String, override val parent: Option[Instruction]) extends Declaration
 
+sealed trait Call extends Expression
+case class FunctionCall(override val parent: Option[Instruction]) extends Call
+
+case class IntExpression(val value: Int, override val parent: Option[Instruction]) extends Expression
+
 abstract sealed class FunctionBody {
   val body: List[Instruction]
 }
@@ -88,6 +129,21 @@ abstract sealed class FunctionBody {
 sealed trait FunctionBodyType
 case object ExpressionBody extends FunctionBodyType
 case object RegularFunctionBody extends FunctionBodyType
+
+
+object ExpressionType {
+  def apply(ctx: ExpressionContext, parent: Option[Instruction]): Expression = {
+    val text = ctx.getText
+    if(text.endsWith(")")) {
+      FunctionCall(parent)
+    } else {
+      val triedInt = Try(text.toInt)
+      if(triedInt.isSuccess) {
+        IntExpression(triedInt.get, parent)
+      } else throw new IllegalStateException(s"Cannot use $text")
+    }
+  }
+}
 
 object FunctionBodyType {
   def apply(ctx: FunctionBodyContext): FunctionBodyType = {
--- src/test/scala/ast/AstSpec.scala
+++ src/test/scala/ast/AstSpec.scala
@@ -39,17 +39,48 @@ class TopLevelAstSpec extends FlatSpec {
 }
 
 
-class NestedAstSpec extends FlatSpec {
+class ClassLevelAstSpec extends FlatSpec {
 
-  "A top level function" should "be parsed correctly" in {
+  def commonAssertions(ast: List[Instruction]): Unit = {
+    assert(ast.size == 1)
+    val classDef = ast.head
+    assert(classDef.isInstanceOf[Clazz])
+    assert(classDef.asInstanceOf[Clazz].name == "Foo")
+    assert(classDef.parent.isEmpty)
+    assert(classDef.children.size == 1)
+  }
+
+  "A class level expression body function" should "be parsed correctly" in {
     val code = "class Foo { fun bar() = println(1) }"
     val ast = new AstCreator().create(code)
-    assert(ast.size == 1)
-    assert(ast.head.isInstanceOf[Clazz])
-    assert(ast.head.asInstanceOf[Clazz].name == "Foo")
-    assert(ast.head.parent.isEmpty)
-    assert(ast.head.children.size == 1)
+    commonAssertions(ast)
     assert(ast.head.children.head.asInstanceOf[Function].bodyType == ExpressionBody)
+  }
+
+  "A regular class level function" should "be parsed correctly" in {
+    val code = "class Foo { fun bar() { println(1) } }"
+    val ast = new AstCreator().create(code)
+    commonAssertions(ast)
+    assert(ast.head.children.head.asInstanceOf[Function].bodyType == RegularFunctionBody)
+  }
+
+
+  "A complex class level function" should "be parsed correctly" in {
+    val code = """
+              |class Foo {
+              |    fun bar() {
+              |         fun foo() = println(1)
+              |         foo()
+              |    }
+              |}""".stripMargin
+
+    val ast = new AstCreator().create(code)
+    commonAssertions(ast)
+    val barFunction = ast.head.children.head.asInstanceOf[Function]
+    assert(barFunction.bodyType == RegularFunctionBody)
+    assert(barFunction.children.size == 2)
+    assert(barFunction.children.head.asInstanceOf[Function].name == "foo")
+    assert(barFunction.children.get(1).get.isInstanceOf[FunctionCall])
   }
 
 }
