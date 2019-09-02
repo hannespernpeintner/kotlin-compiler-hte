b31cdc06fc619512687074deea229a555e347bc7
Hannes Tenter <tenter@e-spirit.com>
2019-09-02 17:56:45 +0200

    restructured property definition methods

--- src/main/scala/compiler/BytecodeCompiler.scala
+++ src/main/scala/compiler/BytecodeCompiler.scala
@@ -4,6 +4,7 @@ import ast.{Clazz, File, Property}
 import jdk.internal.org.objectweb.asm.Opcodes._
 import org.objectweb.asm.{ClassWriter, Opcodes, Type}
 import org.objectweb.asm.tree.ClassNode
+import Extensions.ClassNodeExtension
 
 case class ClassFile(val fileName: String, val content: Array[Byte])
 
@@ -16,40 +17,10 @@ class BytecodeCompiler {
       cn.superName = "java/lang/Object"
       cn.name = clazz.name
 
-      val constructor = cn.visitMethod(ACC_PUBLIC, "<init>", "()V", null, Array())
-      constructor.visitCode()
-      constructor.visitIntInsn(Opcodes.ALOAD, 0)
-      constructor.visitMethodInsn(INVOKESPECIAL, "java/lang/Object", "<init>", "()V", false)
-      clazz.children.foreach {
-        case property: Property =>
-          constructor.visitIntInsn(Opcodes.ALOAD, 0)
-          constructor.visitLdcInsn("Currently only strings")
-          constructor.visitFieldInsn(Opcodes.PUTFIELD, clazz.name, property.name, "Ljava/lang/String;")
-      }
-      constructor.visitInsn(Opcodes.RETURN)
-      constructor.visitEnd()
+      cn.defineNoArgsConstructor(clazz)
 
       clazz.children.foreach {
-        case property: Property => {
-          cn.visitField(ACC_PRIVATE, property.name, "Ljava/lang/String;", null, "Currently only strings").visitEnd()
-          val postFix = s"${property.name.charAt(0).toUpper}${property.name.substring(1, property.name.length)}"
-          val getterName = s"get$postFix"
-          val getterMethodVisitor = cn.visitMethod(ACC_PUBLIC, getterName, "()Ljava/lang/String;", null, Array())
-          getterMethodVisitor.visitCode()
-          getterMethodVisitor.visitIntInsn(Opcodes.ALOAD, 0)
-          getterMethodVisitor.visitFieldInsn(Opcodes.GETFIELD, clazz.name, property.name, "Ljava/lang/String;")
-          getterMethodVisitor.visitInsn(Opcodes.ARETURN)
-          getterMethodVisitor.visitEnd()
-
-          val setterName = s"set$postFix"
-          val setterMethodVisitor = cn.visitMethod(ACC_PUBLIC, setterName, "(Ljava/lang/String;)V", null, Array())
-          setterMethodVisitor.visitCode()
-          setterMethodVisitor.visitIntInsn(Opcodes.ALOAD, 0)
-          setterMethodVisitor.visitIntInsn(Opcodes.ALOAD, 1)
-          setterMethodVisitor.visitFieldInsn(Opcodes.PUTFIELD, clazz.name, property.name, "Ljava/lang/String;")
-          setterMethodVisitor.visitInsn(Opcodes.RETURN)
-          setterMethodVisitor.visitEnd()
-        }
+        case property: Property => cn.defineProperty(property)
       }
       cn.visitEnd()
 
